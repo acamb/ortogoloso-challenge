d3bb788767f608a33e1872cfcb2d967b84d8e61a
andrea cambieri <andrea.cambieri@gmail.com>
2022-09-20 23:09:44 +0200

    AOP logging

---
+++ server/src/main/java/ac/challenge/ortogoloso/configuration/LoggingConfiguration.java
@@ -0,0 +1,29 @@
+package ac.challenge.ortogoloso.configuration;
+
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoggingConfiguration {
+
+    @Bean
+    @Autowired
+    public Advisor controllerLogger(LoggingInterceptor interceptor){
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* ac.challenge.ortogoloso.controller.*.*(..))");
+        return new DefaultPointcutAdvisor(pointcut, interceptor);
+    }
+
+    @Bean
+    @Autowired
+    public Advisor serviceLogger(LoggingInterceptor interceptor){
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* ac.challenge.ortogoloso.service.*.*(..))");
+        return new DefaultPointcutAdvisor(pointcut, interceptor);
+    }
+
+}
