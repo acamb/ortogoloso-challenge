48dc67b2616e9588ffe4d23cd5f4e34b00fefff1
andrea cambieri <andrea.cambieri@gmail.com>
2022-09-20 22:31:53 +0200

    scheletro backend

---
+++ server/src/main/java/ac/challenge/ortogoloso/OrtogolosoApplication.java
@@ -0,0 +1,13 @@
+package ac.challenge.ortogoloso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrtogolosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrtogolosoApplication.class, args);
+	}
+
+}
